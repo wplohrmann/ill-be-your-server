e7c599b83d7adb718697b5e0d9c8551e24a35e81
William Lohrmann <wlohrmann@oni.bio>
2020-08-30 22:50:00 +0100

    Create form from model

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:recipe_id>/', views.detail, name='detail'),
+    path('new', views.new, name='new'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+from urllib import parse
+from .models import Recipe
+from .forms import RecipeForm
+import json
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the recipes index.")
+    recipes = Recipe.objects.all()
+    context = {"recipes": recipes}
+    return render(request, "recipes/index.html", context)
+
+def detail(request, recipe_id):
+    context = {"recipe": get_object_or_404(Recipe, pk=recipe_id)}
+    return render(request, "recipes/detail.html", context)
+
+def new(request):
+    if request.method == "POST":
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            new_recipe = Recipe(**form.cleaned_data)
+            new_recipe.save()
+            return HttpResponse('Thanks! <a href="/recipes/">Back to main page</a>')
+        else:
+            return HttpResponse("Bad form!")
+    else:
+        form = RecipeForm()
+        return render(request, "recipes/new.html", {"form": form})
