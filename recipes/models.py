5da2e2274a40f7fcd625c28fe2c97b92c3be4e43
William Lohrmann <wlohrmann@oni.bio>
2020-08-30 20:58:10 +0100

    Start adding models + admin access

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    instructions = models.TextField()
+    ingredients = models.JSONField()
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    source = models.URLField()
+
